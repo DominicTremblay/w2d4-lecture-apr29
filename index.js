9d1f990ef4b522708522d89a99ecbd19d7a8fe4e
Dominic Tremblay <dominictremblay24@gmail.com>
2019-05-09 12:00:02 -0400

    final demo apr29

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const uuid = require('uuid/v4');
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(cookieParser());
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine', 'ejs');
@@ -42,7 +45,7 @@ const quoteComments = {
     quoteId: 'd9424e04-9df6-4b76-86cc-9069ca8ee4bb',
   },
 };
-const users = {
+const usersDb = {
   'eb849b1f-4642-4c16-a77b-71ac2f90996f': {
     id: 'eb849b1f-4642-4c16-a77b-71ac2f90996f',
     name: 'Kent Cook',
@@ -72,7 +75,7 @@ var getQuotes = quotes => {
 };
 
 // Updating quote in movieQuotesDb
-var updateQuote = (id, quote) => {
+const updateQuote = (id, quote) => {
   if (movieQuotesDb[id]) {
     movieQuotesDb[id].quote = quote;
     return true;
@@ -81,13 +84,122 @@ var updateQuote = (id, quote) => {
   }
 };
 
-console.log(getQuotes(quotesDb));
+const addNewUser = (name, email, password) => {
+  // generate an random id
+  const id = uuid();
+
+  // create a new user object
+  const newUser = {
+    id,
+    name,
+    email,
+    password,
+  };
+  // Add it to the usersDb
+  usersDb[id] = newUser;
+  // return the id
+  return id;
+};
+
+// finding the user object with a loop throught the usersDb
+const findUser = email => {
+  for (const userId in usersDb) {
+    if (usersDb[userId].email === email) {
+      return usersDb[userId];
+    }
+  }
+  return false;
+};
+
+// finding the userObj in usersDb with the builtin find function
+// const findUser = email =>
+//   Object.values(usersDb).find(userObj => userObj.email === email)
+
+const authenticateUser = (email, password) => {
+  // find the user with that email
+  const user = findUser(email);
+  // If the user is found, then compare the passwords
+  if (user && user.password === password) {
+    // if it's a match, return the user id
+    return user.id;
+  } else {
+    // if no match, return false
+    return false;
+  }
+};
+
+const getCurrentUser = req => {
+  // read the user id from the cookies
+  const userId = req.cookies['user_id'];
+  // return the user from usersDb with that id
+  return usersDb[userId];
+};
+
+// Display the login form
+app.get('/login', (req, res) => {
+  const templateVars = { currentUser: null };
+  res.render('login', templateVars);
+});
+
+app.post('/login', (req, res) => {
+  // extract the email and password from the login form
+  const { email, password } = req.body;
+
+  // Authenticate the user
+
+  const userId = authenticateUser(email, password);
+
+  // set the cookie to log the user with their userId
+  if (userId) {
+    res.cookie('user_id', userId);
+    // redirect to main page
+    res.redirect('/quotes');
+  } else {
+    res.send('wrong credentials');
+  }
+});
+
+// Display the signup form
+app.get('/register', (req, res) => {
+  const templateVars = { currentUser: null };
+  res.render('register', templateVars);
+});
+
+// Creating a new user
+app.post('/register', (req, res) => {
+  // extract name, email, password from the form submission
+  // ES6 destructuring
+  const { name, email, password } = req.body;
+
+  // Regular way
+  // const name = req.body.name;
+  // const email = req.body.email;
+  // const password = req.body.password;
+
+  // add a new user to the database
+  const userId = addNewUser(name, email, password);
+
+  // set the cookie for that user
+  res.cookie('user_id', userId);
+  // redirect to the main page
+
+  res.redirect('/quotes');
+});
+
+app.post('/logout', (req, res) => {
+  res.cookie('user_id', null);
+  res.redirect('/login');
+});
 
 app.get('/', (req, res) => res.status(301).redirect('/quotes'));
 
 // Display a list of quotes
 app.get('/quotes', (req, res) => {
-  const templateVars = { quoteList: getQuotes(quotesDb) };
+  const templateVars = {
+    quoteList: getQuotes(quotesDb),
+    currentUser: getCurrentUser(req),
+  };
+  console.log(usersDb);
   res.render('quotes', templateVars);
 });
 
